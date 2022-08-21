8a1836f75ae3b9be621d8a9bcd96365da8cad0a2
Patrik Jurak <patrik.jurak@rearm.hr>
2022-08-21 21:31:05 +0200

    Test commit

---
+++ src/components/NavigationBar/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="navigation-container">
+      <h1>This is navigation bar</h1>
+    </div>
+  );
+};
+
+export default Navigation;
